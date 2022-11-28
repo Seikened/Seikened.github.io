3f8e5575f106b5e534e18f07033b63f8833d8ba3
Seikened <fernanlee2131@gmail.com>
2022-11-28 00:29:08 -0600

    v.09 Validación en el login del proyecto Masterdots🔵

--- proyects/Masterdots/js/app.js
+++ proyects/Masterdots/js/app.js
@@ -1,10 +1,29 @@
-function piramide(numPisos){
-    for(let i =0; i < numPisos; i++){
-        let piso ='';
-        for(let j =0; j<i; j++){
-            piso = piso + '*';
-        }
-        console.log(piso);
+/*
+*  js comrpbation of the data of input
+*/ 
+// Inicializadores de var, objetos, DOM
+const formEntrada = document.getElementById("formEntrada");
+const nickInput = document.getElementById("nick");
+const emailInput = document.getElementById("emal");
+const sizeInput = document.getElementById("size");
+
+
+// Funciones de eventos
+function comprobarFurmulario(event){
+    // Comprobar cambios
+    if(nickInput.value.length==0){
+        console.log("Introduce un nick");
+        nickInput.focus();
+        event.preventDefault();
+        return false; // para que no se envie el formulario
+    }else if(sizeInput.value=="0"){
+        console.log("Introduce un tamaño");
+        sizeInput.focus();
+        event.preventDefault();
+        return false; // para que no se envie el formulario
     }
-}
-piramide(5);
+    return true; // para que se envie el formulario
+} // fin de comprobarFurmulario
+
+// Inicio de carga de eventos
+formEntrada.addEventListener('submit', comprobarFurmulario);

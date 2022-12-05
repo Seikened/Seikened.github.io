fa2111df858d68e476c914f448d53fbf81282b1b
Fer Leon <91928331+Seikened@users.noreply.github.com>
2022-12-05 09:44:55 -0600

    v.12 🔹Geolocalización dentro del proyecto de Masterdots🔵 🔹Optimización de código haciendo un DOM loaded al iniciar la el index de Masterdots🔵 🔹Creación e implementación de un Drag and drop dentro de Masterdots🔵 🔹Implementación de un canvas API dentro de Masterdots🔵 🔹Juego de Masterdots🔵 (No esta terminado y hay bugs)

--- proyects/Masterdots/js/app.js
+++ proyects/Masterdots/js/app.js
@@ -2,40 +2,77 @@
 *  js comrpbation of the data of input
 */ 
 // Inicializadores de var, objetos, DOM
-const formEntrada = document.getElementById("formEntrada");
-const nickInput = document.getElementById("nick");
-const emailInput = document.getElementById("email");
-const sizeInput = document.getElementById("size");
-const errorDisplay = document.getElementById("error")
-
-// Comprobar si hay algun error en el juego.html
-if(sessionStorage.getItem('error')){
-    errorDisplay.innerText = sessionStorage.getItem('error');
-    sessionStorage.removeItem('error');
-}
+var formEntrada;
+var nickInput;
+var emailInput;
+var sizeInput;
+var errorDisplay;
+var avatarItems;
+var itemImg;
+var avatarContainer;
 
 // Funciones de eventos
+/**
+ * @param  {EventObjets} event Evento que salta al realizar el submit
+ */
 function comprobarFurmulario(event){
     // Comprobar cambios
-    if(nickInput.value.match(/(?<!\S)[0-9]/)){
+    if(nickInput.value.match(/(?<!\S)[0-9]/)){ // Comprueba si hay un numero en el nick
         console.log("Introduce un nick");
         nickInput.focus();
         event.preventDefault();
         errorDisplay.innerText = "El nick no puede comenzar con un número";
         return false; // para que no se envie el formulario
     }else if(sizeInput.value=="0"){
-        console.log("Introduce un tamaño");
         sizeInput.focus();
         event.preventDefault();
         errorDisplay.innerText = "Selecciona un tamaño";
         return false; // para que no se envie el formulario
     }
-    errorDisplay.innerText = "";
+
     //Información de usuario es correcta podre utilizar la función de almacenamiento de datos
-    userData(nickInput);
+    userData(nickInput,sizeInput,emailInput,avatarContainer);
     historicalUsers(nickInput);
     return true; // para que se envie el formulario
-} // fin de comprobarFurmulario
+}
+
+function moviendoImg(event){
+    itemImg=event.target;
+}
+
+function cambiarImg(){
+    avatarContainer.src=itemImg.src;
+}
+/**
+ * Carga de objetos del DOM comprobaciones y eventos del formulario
+ */
+function DOMCargado(){
+    // Captura de todos Elements necesarios
+    formEntrada = document.getElementById("formEntrada");
+    nickInput = document.getElementById("nick");
+    emailInput = document.getElementById("email");
+    sizeInput = document.getElementById("size");
+    errorDisplay = document.getElementById("error");
+
+    // Comrpbar si hay algun error de juego.html
+    if(sessionStorage.getItem('error')!=null){
+        error.innerText = sessionStorage.getItem('error');
+        sessionStorage.removeItem('error');
+    }
+    
+    formEntrada.addEventListener('submit', comprobarFurmulario);
+
+    avatarItems=document.getElementsByClassName("avatarImgItem");
+    // Eventos del Drag an drop
+    for(let item of avatarItems){
+        item.addEventListener('dragstart',moviendoImg);
+    }
+    avatarContainer=document.getElementById("avatarImg");
+    avatarContainer.addEventListener('dragover',e=>(e.preventDefault()));
+    avatarContainer.addEventListener('drop',cambiarImg);
+}
 
 // Inicio de carga de eventos
-formEntrada.addEventListener('submit', comprobarFurmulario);
+document.addEventListener('DOMContentLoaded', DOMCargado);
+// GeoLocalización 
+dataGeolocaization();
--- proyects/Masterdots/js/userData.js
+++ proyects/Masterdots/js/userData.js
@@ -1,14 +1,41 @@
 // JS Para la generación de los datos de usuario
+
+/*
+* @autor: Fernando Leon Franco <leon@leonesfrancos.com>
+* {@link https://github.com/Seikened/Seikened.github.io GitHub}
+*/
+
 var nick;
+var size;
+var email;
+var geolocalizacionTxt;
+var avatarImg;
 
-//  sessionStorage
-function userData(nick){
+//sessionStorage
+/**
+ * Alamacena los datos del usuario en el sessionStorage
+ * @param  {HTMLElement} nick nick del usuario
+ * @param  {HTMLElement} size tamaño del panel
+ * @param  {HTMLElement} email email del usuario
+ * @param  {HTMLElement} avatarContainer imagen del avatar
+ */
+function userData(nick,size,email,avatarContainer){
     sessionStorage.setItem('nick',nick.value); // Guarda el nick en el sessionStorage en un formato lista
+    sessionStorage.setItem('size',size.value); // Guarda el size en el sessionStorage en un formato lista
+    sessionStorage.setItem('email',email.value); // Guarda el email en el sessionStorage en un formato lista
+    sessionStorage.setItem('geolocalizacionTxt',geolocalizacionTxt); // Guarda la geolocalizacion en el sessionStorage en un formato lista
+    sessionStorage.setItem('avatarImg',avatarContainer.src); // Guarda el avatar en el sessionStorage en un formato lista
 }
-
+/**
+ * Obtiene los datos del usuario del sessionStorage
+ */
 function getUserData(){
     nick = sessionStorage.getItem('nick');
-    console.log(nick);
+    size = sessionStorage.getItem('size');
+    email = sessionStorage.getItem('email');
+    geolocalizacionTxt = sessionStorage.getItem('geolocalizacionTxt');
+    avatarImg = sessionStorage.getItem('avatarImg');
+    
 }
 
 function checkUserData(){
@@ -18,7 +45,18 @@ function checkUserData(){
     }
     return true;
 }
-
+function dataGeolocaization(){
+    if(!navigator.geolocation){
+        geolocalizacionTxt = "No esposible o el navegador no es compatible";
+    }else{
+        navigator.geolocation.getCurrentPosition(
+            // Exitoso
+            (position)=>{geolocalizacionTxt='Latitud: '+ position.coords.latitude+' , Longitud: '+position.coords.longitude},
+            // Error
+            ()=>{geolocalizacionTxt="La geolocalización no se ha podido realizar";}
+        )
+    }
+}
 //  localStorage
 function historicalUsers(nick){
     let historicoStorage = localStorage.getItem('historico');

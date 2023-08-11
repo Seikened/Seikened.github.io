6c21cd8bb7c631b8355658967199326ef17ac2b6
Seikened <fernanlee2131@gmail.com>
2023-08-11 09:34:18 -0600

    test para ver si funciona

--- proyects/proyect_n/proyecto_API/proyecto_API.js
+++ proyects/proyect_n/proyecto_API/proyecto_API.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-// mongoose.connect('mongodb+srv://seiken:fer213142@cluster0.va3sdo5.mongodb.net/miapp?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://seiken:fer213142@cluster0.va3sdo5.mongodb.net/miapp?retryWrites=true&w=majority');
 
 app.get('/' , user.list)
 app.post('/', user.create) 
@@ -19,5 +19,5 @@ app.get('*', (req,res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log('Servidor corriendo en el puerto ' + port + '...')
 });
